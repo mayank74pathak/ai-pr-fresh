101d7dce9d97e145c65aabb8d097ccc2ffead9df
Mayank <mayank74pathak@gmail.com>
2026-04-20 23:18:46 +0530

    trigger AI PR22

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function sum(a, b) {
-  return a + ;
+  return a ++ b;   // bug
 }
 
 console.log(sum(5)); // missing argument
 
-if ((x = 10)) {
-  // ❌ assignment instead of comparison
+if (x = 10) {      // bug
   console.log("x is 10");
+}
