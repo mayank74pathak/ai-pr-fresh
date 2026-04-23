8e9832815859e5bdbc29c07673678647ea6c1913
Mayank <mayank74pathak@gmail.com>
2026-04-23 23:27:36 +0530

    trigger AI PR25

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function sum(a, b) {
-  return a ++ b;   // bug
+  return a + b;   // bug
 }
 
 console.log(sum(5)); // missing argument
 
-if (x -= 10) {      // bug
+if (x = 10) {      // bug
   console.log("new 900007");
 }
